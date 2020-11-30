7324290c6f304f57e83953c35d43a0c96228fb04
hafid id-baha <hafidsoftdev@gmail.com>
2020-11-30 18:14:12 +0100

    create hafid card application

--- ninjacard/lib/main.dart
+++ ninjacard/lib/main.dart
@@ -8,10 +8,85 @@ class Card extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          title: Text("Ninja Card"),
+          title: Text("Hafid Card"),
           centerTitle: true,
-          backgroundColor: Colors.grey[600],
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 40, 30, 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/user.jpg'),
+                  radius: 40,
+                ),
+              ),
+              Divider(
+                height: 60,
+                color: Colors.grey[800],
+              ),
+              Text(
+                "Name",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                  fontSize: 15,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Hafid Id-baha",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: Colors.yellow[400],
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Current Level",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                  fontSize: 15,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "10",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: Colors.yellow[400],
+                  fontSize: 20,
+                ),
+              ),
+              Divider(
+                height: 60,
+                color: Colors.grey[800],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.mail,
+                    color: Colors.greenAccent,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "hafid.dev@gmail.com",
+                      style:TextStyle(
+                        color: Colors.greenAccent,
+                      ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
