eb3cbb60ea1a58bac76a2cb42941aa170aba8489
hafid id-baha <hafidsoftdev@gmail.com>
2020-12-01 16:46:06 +0100

    using stateful instead of statless widget

--- ninjacard/lib/main.dart
+++ ninjacard/lib/main.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(Card());
 }
-class Card extends StatelessWidget {
+class Card extends StatefulWidget {
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
