9416892ad0e73609866a09581dcf2d01443b26a2
hafid id-baha <hafidsoftdev@gmail.com>
2020-12-01 17:43:38 +0100

    showing a list of data using map function

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -12,9 +12,23 @@ class QuotesList extends StatefulWidget {
 }
 
 class _QuotesListState extends State<QuotesList> {
+  List<String> quotes = [
+    "It Doesn’t Matter Where You Came From. All That Matters Is Where You Are Going.",
+    "Think Big And Don’t Listen To People Who Tell You It Can’t Be Done. Life’s Too Short To Think Small",
+    "You Can Develop Any Habit Or Thought Or Behavior That You Consider Desirable Or Necessary."
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awsome Quotes"),
+        backgroundColor: Colors.black26,
+      ),
+      body: Column(
+        children: quotes.map((q) => Text(q)).toList(),
+      ),
+    );
   }
 }
 
