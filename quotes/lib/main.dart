c894ae4cb11655eaca65b63ae4748a4c63fd7a99
hafid id-baha <hafidsoftdev@gmail.com>
2020-12-01 19:01:39 +0100

    using card widget to show all the quotes with the auhtors

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -18,6 +18,35 @@ class _QuotesListState extends State<QuotesList> {
     Quote(text:"Think Big And Don’t Listen To People Who Tell You It Can’t Be Done. Life’s Too Short To Think Small",author: "hafid id-baha" ),
     Quote(text:"You Can Develop Any Habit Or Thought Or Behavior That You Consider Desirable Or Necessary.",author: "samad jalil" )
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(
+                color: Colors.blueAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +56,7 @@ class _QuotesListState extends State<QuotesList> {
         backgroundColor: Colors.black26,
       ),
       body: Column(
-        children: quotes.map((q) => Text("${q.text} - ${q.author}")).toList(),
+        children: quotes.map((q) => quoteTemplate(q)).toList(),
       ),
     );
   }
