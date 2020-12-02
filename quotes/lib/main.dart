594273de52fd1f7497bc92bb76fd607bbeb9220f
hafid id-baha <hafidsoftdev@gmail.com>
2020-12-02 17:22:51 +0100

    refactoring quote card to its own widget

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'qoute_card.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -19,33 +20,6 @@ class _QuotesListState extends State<QuotesList> {
     Quote(text:"You Can Develop Any Habit Or Thought Or Behavior That You Consider Desirable Or Necessary.",author: "samad jalil" )
   ];
 
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              quote.text,
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontSize: 18,
-              ),
-            ),
-            SizedBox(height: 6),
-            Text(
-              quote.author,
-              style: TextStyle(
-                color: Colors.blueAccent,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,10 +30,13 @@ class _QuotesListState extends State<QuotesList> {
         backgroundColor: Colors.black26,
       ),
       body: Column(
-        children: quotes.map((q) => quoteTemplate(q)).toList(),
+        children: quotes.map((q) => QuoteCard(quote: q)).toList(),
       ),
     );
   }
 }
+
+
+
 
 
