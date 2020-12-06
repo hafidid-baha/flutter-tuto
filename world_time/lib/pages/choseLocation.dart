9e378b539f26173ff16fad67c1ac1be2888e7a43
hafid id-baha <hafidsoftdev@gmail.com>
2020-12-06 18:37:54 +0100

    showing all the locations inside listview

--- world_time/lib/pages/choseLocation.dart
+++ world_time/lib/pages/choseLocation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChoseLocation extends StatefulWidget {
   @override
@@ -6,6 +7,18 @@ class ChoseLocation extends StatefulWidget {
 }
 
 class _ChoseLocationState extends State<ChoseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago'),
+    WorldTime(url: 'America/New_York', location: 'New York'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +26,17 @@ class _ChoseLocationState extends State<ChoseLocation> {
         backgroundColor: Colors.blue,
         title: Text("Change Location"),
       ),
-      body: SafeArea(
-        child: Text('this is the chose location page '),
-      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context,index){
+          return Card(
+            child: ListTile(
+              onTap: (){},
+              title: Text(locations[index].location),
+            ),
+          );
+        },
+      )
     );
   }
 }
