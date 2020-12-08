a1152c730c3b16b26a700169f53f1bbe698e2929
hafid id-baha <hafidsoftdev@gmail.com>
2020-12-08 16:03:32 +0100

    first commit

---
+++ lazy_loading/android/app/src/main/kotlin/agh/hafid/lazy_loading/MainActivity.kt
@@ -0,0 +1,6 @@
+package agh.hafid.lazy_loading
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lazy_loading/lib/imageItem.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class ImageItem{
+  final String content,url;
+  ImageItem({this.url,this.content});
+}
